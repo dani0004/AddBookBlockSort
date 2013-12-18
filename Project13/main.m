f840e0558ed7a61cf70a12c1858113f385c900ca
dani0004 <dani0004@algonquinlive.com>
2013-12-18 15:59:06 -0500

    Initial Commit

---
+++ Project13/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Project13
+//
+//  Created by Wendy-Anne Daniel on 2013-12-18.
+//  Copyright (c) 2013 Wendy-Anne Daniel. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
